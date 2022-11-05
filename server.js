1f1d37d590cd4c156cbfa73016201765d874c33e
Artem Solovyov <solovevartem892@gmail.com>
2022-11-05 12:11:21 +0200

    app nodejs

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const methodOverride = require('method-override');
+const postRoutes = require('./routes/post-routes');
+const postApiRoutes = require('./routes/api-post-routes');
+const contactRoutes = require('./routes/contact-routes');
+const createPath = require('./helpers/create-path');
+
+const errorMsg = chalk.bgKeyword('white').redBright;
+const successMsg = chalk.bgKeyword('green').white;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+const PORT = 5000;
+const db =
+    "mongodb+srv://Artem:9005Artem@cluster0.bfyibct.mongodb.net/Probe_1?retryWrites=true&w=majority";
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log(successMsg('Connected to DB')))
+  .catch((error) => console.log(errorMsg(error)));
+
+app.listen(PORT, (error) => {
+  error ? console.log(errorMsg(error)) : console.log(successMsg(`listening port ${PORT}`));
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  const title = 'Home';
+  res.render(createPath('index'), { title });
+});
+
+app.use(postRoutes);
+app.use(contactRoutes);
+app.use(postApiRoutes);
+
+app.use((req, res) => {
+  const title = 'Error Page';
+  res
+    .status(404)
+    .render(createPath('error'), { title });
+});
